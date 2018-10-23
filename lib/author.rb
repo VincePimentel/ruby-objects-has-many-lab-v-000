f2a24672161513877251c156256e9a97a26c2fb0
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 21:27:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_accessor :name, :posts
 
-  #@@post_count = 0
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -11,13 +11,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    #@@post_count += 1
+    @@post_count += 1
   end
 
   def add_post_by_title(post)
     post = Post.new(post)
     @posts << post
     post.author = self
-    #@@post_count += 1
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count    
   end
 end
