0b5637d4313cad58ce9b0b730eac75cfabc977a6
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 21:12:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
-  attr_accessor :name, :songs, :song_count
+  attr_accessor :name, :songs
+
+  @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = Array.new
-    @song_count += 1
   end
 
   def add_song(song)
