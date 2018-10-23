40e77dddc771d90b3550dd659a7db09a12371518
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 21:12:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,17 +11,17 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(song)
     song = Song.new(song)
     @songs << song
     song.artist = self
-    @song_count += 1
+    @@song_count += 1
   end
 
   def self.song_count
-    @song_count
+    @@song_count
   end
 end
