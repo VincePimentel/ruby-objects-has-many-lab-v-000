95fe6db75efe14c0b2943443cd50578a1c76d1dd
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 21:05:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,11 @@ class Song
 
   def initialize(name)
     @name = name
-    #@@all << self
+    @@all << self
   end
 
   def artist_name
-    self.artist.name
+    self.artist || nil
   end
 
   def self.all
